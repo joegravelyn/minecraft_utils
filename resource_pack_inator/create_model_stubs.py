539cfb2029ed9f4e71c56269e85b99468b0022d4
joegravelyn <joe@joegravelyn.com>
2025-10-20 16:30:06 -0700

    script to make stub files from png files using the same folder structure

---
+++ resource_pack_inator/create_model_stubs.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+import pandas as pd
+import json
+
+def main():
+   rpi_dir = Path.cwd().joinpath("resource_pack_inator")
+   output_dir = rpi_dir.joinpath("output")
+
+   # Input asset folder that contains sorted custom textures
+   texture_dir = Path(input("Assets folder: "))
+
+   for namespace_folder in texture_dir.iterdir():
+      namespace = namespace_folder.name
+
+      if namespace == "minecraft": continue
+
+      t_dir = namespace_folder.joinpath("textures")
+      for t_file in t_dir.glob("**/*.png"):
+         item_parts = str(t_file.relative_to(t_dir)).removesuffix(".png").split("\\")
+         model = {"parent": "item/generated", "textures": {"layer0": f"{namespace}:{"/".join(item_parts)}"}}
+         o_dir = output_dir.joinpath(namespace, "models", *item_parts[:-1])
+         o_dir.mkdir(exist_ok=True, parents=True)
+         o_dir.joinpath(f"{item_parts[-1]}.json").write_text(json.dumps(model, indent=3))
+
+
+def clear_dir(p: Path):
+   if p.is_file():
+      p.unlink()
+   else:
+      for d in p.iterdir():
+         clear_dir(d)
+      p.rmdir()
+
+if __name__ == "__main__":
+   main()
