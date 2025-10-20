9305ce1a0d2355c0d772dd8979b745e63900e506
joegravelyn <joe@joegravelyn.com>
2025-10-20 16:31:40 -0700

    splitting main function out to get input

--- resource_pack_inator/create_model_stubs.py
+++ resource_pack_inator/create_model_stubs.py
@@ -3,11 +3,13 @@ import pandas as pd
 import json
 
 def main():
-   rpi_dir = Path.cwd().joinpath("resource_pack_inator")
-   output_dir = rpi_dir.joinpath("output")
-
    # Input asset folder that contains sorted custom textures
    texture_dir = Path(input("Assets folder: "))
+   create_stubs(texture_dir)
+
+def create_stubs(texture_dir):
+   rpi_dir = Path.cwd().joinpath("resource_pack_inator")
+   output_dir = rpi_dir.joinpath("output")
 
    for namespace_folder in texture_dir.iterdir():
       namespace = namespace_folder.name
@@ -21,15 +23,6 @@ def main():
          o_dir = output_dir.joinpath(namespace, "models", *item_parts[:-1])
          o_dir.mkdir(exist_ok=True, parents=True)
          o_dir.joinpath(f"{item_parts[-1]}.json").write_text(json.dumps(model, indent=3))
-
-
-def clear_dir(p: Path):
-   if p.is_file():
-      p.unlink()
-   else:
-      for d in p.iterdir():
-         clear_dir(d)
-      p.rmdir()
 
 if __name__ == "__main__":
    main()
