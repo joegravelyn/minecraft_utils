d5177265f4142574cd368b172660b9414d4a242f
joegravelyn <joe@joegravelyn.com>
2026-01-28 09:01:54 -0800

    screenshot grabbing now stores last run to filter pics before hashing

--- screenshot_grab_inator/main.py
+++ screenshot_grab_inator/main.py
@@ -6,7 +6,6 @@ import pandas as pd
 
 
 def main(check_all = False):
-   # Store date so future runs can look at only images created afterwards (check_all param)
    cur_date = datetime.now()
 
    configs = json.loads((Path(__file__).resolve().parent.joinpath("config.json").read_text()))
@@ -15,36 +14,40 @@ def main(check_all = False):
    if not target_dir.exists(): target_dir.mkdir(parents=True)
 
    list_dir = target_dir.joinpath("list.csv")
-   if list_dir.exists():
-      list_df = pd.read_csv(list_dir)
-   else:
-      list_df = pd.DataFrame(columns=["hash","timestamp","name","original_dir"])
+   list_df = pd.read_csv(list_dir) if list_dir.exists() else pd.DataFrame(columns=["hash","timestamp","name","original_dir"])
+
+   date_dir = target_dir.joinpath("last_run.csv")
+   last_date = datetime.fromisoformat(date_dir.read_text()) if date_dir.exists() else datetime(2009, 5, 10)
 
    appdata = Path.home().joinpath("AppData", "Roaming")
 
    if configs["bedrock"]:
       bedrock_user_folder = appdata.joinpath("Minecraft Bedrock", "Users")
       for p in bedrock_user_folder.glob("*/games/com.mojang/Screenshots/*"):
-         list_df = copy_screenshots(p, target_dir, list_df)
+         list_df = copy_screenshots(p, target_dir, list_df, last_date)
 
    if configs["java"]:
       java_folder = appdata.joinpath(".minecraft", "screenshots")
-      list_df = copy_screenshots(java_folder, target_dir, list_df)
+      list_df = copy_screenshots(java_folder, target_dir, list_df, last_date)
 
    if any(configs["other_java_installs"]):
       for i in configs["other_java_installs"]:
          for p in Path(i).glob("*"):
             p_ = p.joinpath("screenshots")
-            list_df = copy_screenshots(p_, target_dir, list_df)
+            list_df = copy_screenshots(p_, target_dir, list_df, last_date)
 
    list_df.to_csv(list_dir, index=False)
+   target_dir.joinpath("last_run.txt").write_text(cur_date.isoformat())
 
 
-def copy_screenshots(mc_path: Path, target_path: Path, check_df: pd.DataFrame) -> pd.DataFrame:
+def copy_screenshots(mc_path: Path, target_path: Path, check_df: pd.DataFrame, check_time: datetime) -> pd.DataFrame:
    for pic in [f for f in mc_path.glob("*") if f.suffix in {".jpeg", ".png"}]:
+      dt = datetime.fromtimestamp(pic.stat().st_mtime)
+      if dt <= check_time: continue
+
       hash = image_hash(pic)
       if any(check_df["hash"] == hash): continue
-      dt = datetime.fromtimestamp(pic.stat().st_mtime)
+      
       dt_count = len(check_df[check_df["timestamp"] == dt])
 
       new_name = dt.strftime("%Y-%m-%d-%H-%M-%S") + (f"-{dt_count}" if dt_count > 0 else "")
